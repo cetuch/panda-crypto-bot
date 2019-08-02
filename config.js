37b94d25042f31d047a6052846934a402e381be3
cetuch <53572644+cetuch@users.noreply.github.com>
2019-08-02 19:28:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
-  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.LINE_CHANNEL_SECRET,
-  imgurClientId: process.env.IMGUR_CLIENT_ID,
+  channelAccessToken: process.env.9EQDL+a4qHdYrNxdDPC/eExmvMrLuaXSJuo6ieNPkFgkJlqeQr2cBJ9+OrPqi9hScSvWQ8EfByd7LFkSXVfM3Td0daGZ9wfmtHnkdU5ETGzDFm3JOiLLj8L17RCdon7qOa2fW8KmugttR5fE+lF1TgdB04t89/1O/w1cDnyilFU=,
+  channelSecret: process.env.9f475a9231fc788a7e83ea1352287f56,
+  imgurClientId: process.env.1605296497,
   cryptoPage2RichMenuId: process.env.CRYPTO_PAGE_2_RICH_MENU_ID,
   verify: false,
   redisUrl: process.env.REDIS_URL,
